6d33e26cc9d844dc25e509a057d0aed8d6b67e2b
Saikumar Eega <118651683+Saieega@users.noreply.github.com>
2023-12-20 23:13:10 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,10 +1,10 @@
 const todoList = () => {
     const all = [];
     const add = (todoItem) => {
-        all.push(todoItem);
+      all.push(todoItem);
     };
     const markAsComplete = (index) => {
-        all[index].completed = true;
+      all[index].completed = true;
     };
 
     let i;
@@ -14,89 +14,92 @@ const todoList = () => {
         
         const over = [];
         for (i = 0; i < all.length; i++) {
-            let date = new Date(all[i].dueDate);
-            if (date.getDate() === todayDate - 1)  {
+           let date = new Date(all[i].dueDate);
+           if (date.getDate() === todayDate - 1)  {
             over.push(all[i]);
-            }
+           }
         }
-        return over;
+        return over;  
     };
-
+  
     const dueToday = () => {
-
-        const due = [];
-        for (i = 0; i < all.length; i++) {
-            let date = new Date(all[i].dueDate);
-        if (date.getDate() === todayDate) {
-            due.push(all[i]);
-        }
+   
+      const due = [];
+      for (i = 0; i < all.length; i++) {
+        let date = new Date(all[i].dueDate);
+      if (date.getDate() === todayDate) {
+        due.push(all[i]);
+      }
     }
     return due;
     };
-
+  
     const dueLater = () => {
+   
         const later = [];
         for (i = 0; i < all.length; i++) {
             let date = new Date(all[i].dueDate);
             if (date.getDate() === todayDate + 1) {
-                later.push(all[i]);
+              later.push(all[i]);
             }
         }
         return later;
     };
-
+  
     const toDisplayableList = (list) => {
 
-        const output = [];
-        for (i = 0; i < list.length; i++) {
-            let date = new Date(list[i].dueDate);
-            const fd = date.toISOString().split("T")[0];
-            if (date.getDate() === todayDate) {
-                if (list[i].completed === false) {
-                    output.push(`[ ] ${list[i].title}`);
-            } else {
-                output.push(`[x] ${list[i].title}`);
-            }
+      const output = [];
+      for (i = 0; i < list.length; i++) {
+        let date = new Date(list[i].dueDate);
+        const fd = date.toISOString().split("T")[0];
+        if (date.getDate() === todayDate) {
+          if (list[i].completed === false) {
+            output.push(`[ ] ${list[i].title}`);
+          } else {
+            output.push(`[x] ${list[i].title}`);
+          }  
         } else {
-            if (list[i].completed === false) {
-                output.push(`[ ] ${list[i].title} ${fd}`);
-            } else {
+          if (list[i].completed === false) {
+            output.push(`[ ] ${list[i].title} ${fd}`);
+          } else {
             output.push(`[x] ${list[i].title} ${fd}`);
-            }
-            }
+          }
+          }
         }
-        return output.join("\n");
+      return output.join("\n");
     };
-
+  
     return {
-        all,
-        overdue,
-        toDisplayableList,
-        add,
-        markAsComplete,
-        dueToday,
-        dueLater,
+      all,
+      overdue,
+      toDisplayableList,
+      add,
+      markAsComplete,
+     
+      dueToday,
+      dueLater,
+     
     };
-    };
-
+  };
+  
   // ####################################### #
   // DO NOT CHANGE ANYTHING BELOW THIS LINE. #
   // ####################################### #
-
+  
 module.exports = todoList;
 const todos = todoList();
 
 const formattedDate = (d) => {
-    return d.toISOString().split("T")[0];
+  return d.toISOString().split("T")[0];
 };
 
 var dateToday = new Date();
 const today = formattedDate(dateToday);
 const yesterday = formattedDate(
-    new Date(new Date().setDate(dateToday.getDate() - 1))
+  new Date(new Date().setDate(dateToday.getDate() - 1))
 );
 const tomorrow = formattedDate(
-    new Date(new Date().setDate(dateToday.getDate() + 1))
+  new Date(new Date().setDate(dateToday.getDate() + 1))
 );
 
 todos.add({ title: "Submit assignment", dueDate: yesterday, completed: false });
